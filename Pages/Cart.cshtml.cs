786c23b7a430a362fe6d43b733d00f6453854a44
Cookzie <timx4192@edu.zealand.dk>
2021-12-14 19:26:11 +0100

    Added Pages

--- Pages/Cart.cshtml.cs
+++ Pages/Cart.cshtml.cs
@@ -2,20 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.ComponentModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Absence.Pages
 {
-    public class LoginModel : PageModel
+    public class CartModel : PageModel
     {
-        
         public void OnGet()
         {
-         
         }
-        
-      
     }
 }
